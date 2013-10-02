563ed72756e2cc355543d5cfbcffd4f9bd4d6d9e
Ridhwana <Ridhwana.khan16@gmail.com>
2013-10-02 10:06:43 +0200

    initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MapsApp::Application.config.secret_token = '9d5f098349e95cd92052c17b33f0ef301342de64c9233c0a0e3e1f36ca383b1c81955c993b06f1c3ede05deffbf201a5363a54dd7f598516bb95f173d69a09f3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+MapsApp::Application.config.session_store :cookie_store, key: '_Maps_app_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# MapsApp::Application.config.session_store :active_record_store
